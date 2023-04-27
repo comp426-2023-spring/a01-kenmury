0d42552541a59c398d813991389c90e276348c8f
Kenny Murillo <kenmury@email.unc.edu>
2023-04-26 21:12:57 -0400

    test

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ import minimist from 'minimist'
 
 var args = minimist(process.argv.slice(2))
 
+console.log(args)
+
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 
