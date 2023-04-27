d2c7deef836f3229784fc2e62fe767e38592d227
Kenny Murillo <kenmury@email.unc.edu>
2023-04-26 22:22:34 -0400

    full test

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ import minimist from 'minimist'
 
 var args = minimist(process.argv.slice(2))
 
-
-
-
-
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 
@@ -57,8 +53,8 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
     });
 
     server.listen(port, hostname, () => {
-        console.log('Server listening on port ${port}');
-    })
+        console.log(`Server listening on port ${port}`);
+    });
 
 
 });
